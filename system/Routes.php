2cd76de78419da3649b6830589f52f701a5933df
Hermine Militonyan <militonyan.hermine@gmail.com>
2020-06-08 17:12:54 +0400

    a small fix

--- system/Routes.php
+++ system/Routes.php
@@ -45,11 +45,11 @@ class Routes {
 				$default_class = "controllers\\Auth";
 				if(class_exists($default_class)){
 					$default_obj = new $default_class;
-					if(method_exists($default_obj, "index")){
+					if(method_exists($default_obj, "login")){
 						$default_obj->index();
 					}
 					else{
-						echo "Method index() of class Auth not found.";
+						echo "Method login of class Auth not found.";
 					}
 				}
 				else{
